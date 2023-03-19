7013740124376643829735897d8dc19bd7c1dae3
Scott Okamoto <swokamoto@gmail.com>
2023-03-19 21:19:33 +0000

    added template index.html

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -8,7 +8,7 @@ def hello():
 
 @app.route("/home")
 def home():
-    return "Welcome Home!"
+    return render_template("index.html")
 
 if __name__=="__main__":
     app.run()
