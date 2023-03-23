eb1587138caafa503903becf797be139cabb88e5
Kyunis99 <Yunis_karim@columbusstate.edu>
2023-03-23 17:52:14 -0400

    Created basic homepage for webapp needs work

--- app.py
+++ app.py
@@ -2,13 +2,11 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello World!"
 
 @app.route("/home")
 def home():
-    return render_template("index.html")
+    return render_template("home.html")
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     app.run(debug=True)
