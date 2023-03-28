1c2fad84f634e8db71acf042f89a7089ae17cc7d
swokamoto <swokamoto@gmail.com>
2023-03-28 17:09:58 -0400

    added settings page to test local storage

--- app.py
+++ app.py
@@ -4,9 +4,10 @@ from newsapi import NewsApiClient
 app = Flask(__name__)
 newsapi = NewsApiClient(api_key='a7ce5af704c4493584f1068248f3b540')
 
+
 @app.route("/home")
 def home():
-    top_headlines = newsapi.get_top_headlines(category='technology', 
+    top_headlines = newsapi.get_top_headlines(category='technology',
                                               language='en',
                                               country='us')
     articles = top_headlines['articles']
